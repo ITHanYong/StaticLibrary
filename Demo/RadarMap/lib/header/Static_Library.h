96489de4f7cd4daf26ed895baf56bb859de23dbf
HanYong <HanYong@192.168.1.101>
2019-01-20 18:23:46 +0800

    静态库.a文件生成

---
+++ Demo/RadarMap/lib/header/Static_Library.h
@@ -0,0 +1,13 @@
+//
+//  Static_Library.h
+//  Static Library
+//
+//  Created by HanYong on 2019/1/20.
+//  Copyright © 2019年 HanYong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Static_Library : NSObject
+
+@end
---
+++ Static Library/Static Library/Static_Library.m
@@ -0,0 +1,13 @@
+//
+//  Static_Library.m
+//  Static Library
+//
+//  Created by HanYong on 2019/1/20.
+//  Copyright © 2019年 HanYong. All rights reserved.
+//
+
+#import "Static_Library.h"
+
+@implementation Static_Library
+
+@end
